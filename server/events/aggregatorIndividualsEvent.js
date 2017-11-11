2ef4f6247713b4285a879a4cbd186660961ec00a
Gilles Perreymond <gperreymond@gmail.com>
2017-11-11 21:07:49 +0100

    remove error on promise handler

--- server/events/aggregatorIndividualsEvent.js
+++ server/events/aggregatorIndividualsEvent.js
@@ -1,10 +1,9 @@
 const path = require('path')
 const async = require('async')
 const ProgressBar = require('progress')
-const glob = require('glob-promise')
+const glob = require('glob')
 const YAML = require('yamljs')
-const Promise = require('bluebird')
-const rp = require('request-promise')
+const rp = require('request')
 
 module.exports = (message, server) => {
   console.log('EVENT_AGGREGATOR_INDIVIDUALS', message)
@@ -20,61 +19,42 @@ module.exports = (message, server) => {
     }
   }
   const databaseDir = path.resolve(__dirname, '../../data/collector/' + database)
-  glob(databaseDir + '/individuals/**/*.yml')
-    .then(files => {
-      return aggregator(server.r, files, databaseConvert[database].key, databaseConvert[database].value)
+  const files = glob.sync(databaseDir + '/individuals/**/*.yml')
+  const bar = new ProgressBar('  aggregators [:bar] :percent :etas', {width: 40, total: files.length})
+  async.eachLimit(files, 50, (file, next) => {
+    aggregator(server.r, file, databaseConvert[database].key, databaseConvert[database].value, () => {
+      bar.tick()
+      next()
     })
-    .then(() => {
-      console.log('DONE callback')
-      if (message.callback !== false) {
-        rp({
-          method: 'POST',
-          uri: message.callback
-        })
-      }
-    })
-    .catch(() => {
-      console.log('DONE callback (error)')
-      if (message.callback !== false) {
-        rp({
-          method: 'POST',
-          uri: message.callback
-        })
-      }
-    })
+  }, () => {
+    console.log('DONE callback')
+    if (message.callback !== false) {
+      rp({
+        method: 'POST',
+        uri: message.callback
+      })
+    }
+  })
 }
 
-const aggregator = function (r, files, key, value) {
-  return new Promise((resolve, reject) => {
-    if (files.length === 0) return resolve()
-    console.log('...', 'aggregator', files.length)
-    const bar = new ProgressBar('  aggregators [:bar] :percent :etas', {width: 40, total: files.length})
-    async.mapLimit(files, 10, (file, next) => {
-      let individual = YAML.load(file)
-      r.db('aggregators').table('individuals')
-        .then(() => {
-          return r.db('aggregators').table('individuals').insert({id: individual.email})
-        })
-        .then(() => {
-          let data = {}
-          data[key] = individual[value]
-          data.createdAt = new Date(individual.createdAt)
-          return r.db('aggregators').table('individuals').get(individual.email).update(data)
-        })
-        .then(() => {
-          bar.tick()
-          next(null)
-        })
-        .catch(error => {
-          bar.tick()
-          next(error)
-        })
-    }, error => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve()
-      }
+const aggregator = function (r, file, key, value, callback) {
+  let individual = YAML.load(file)
+  r.db('aggregators').table('individuals')
+    .then(() => {
+      return r.db('aggregators').table('individuals').insert({id: individual.email})
     })
-  })
+    .then(() => {
+      let data = {}
+      data[key] = individual[value]
+      data.createdAt = new Date(individual.createdAt)
+      return r.db('aggregators').table('individuals').get(individual.email).update(data)
+    })
+    .then(() => {
+      callback()
+      return null
+    })
+    .catch(() => {
+      callback()
+      return null
+    })
 }
